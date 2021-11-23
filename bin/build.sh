06a84d6285ec3d9e4c2c23850e45485114226a11
Clicia Scarlet <yuyuvn@mac.com>
2021-11-24 01:32:30 +0900

    Fix github action syntax

--- bin/build.sh
+++ bin/build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
+set -e
 
 mkdir .out
-rm .out/*
+rm -f .out/*
 GOOS=darwin GOARCH=amd64 go build -o .out/decrypter-$RELEASE_VERSION-darwin-amd64 cmd/decrypter/main.go
 GOOS=linux GOARCH=amd64 go build -o .out/decrypter-$RELEASE_VERSION-linux-amd64 cmd/decrypter/main.go
-GOOS=linux GOARCH=arm go build -o .out/decrypter-$RELEASE_VERSION-linux-arm cmd/rump/decrypter.go
+GOOS=linux GOARCH=arm go build -o .out/decrypter-$RELEASE_VERSION-linux-arm cmd/decrypter/main.go
 GOOS=windows GOARCH=amd64 go build -o .out/decrypter-$RELEASE_VERSION-windows-amd64 cmd/decrypter/main.go
