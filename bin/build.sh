eeb1d83fed7dc6f44ccafc54e105e96298bb019e
Clicia Scarlet <yuyuvn@mac.com>
2023-06-20 00:28:38 +0900

    Remove unused arch

--- bin/build.sh
+++ bin/build.sh
@@ -6,5 +6,5 @@ rm -f .out/*
 GOOS=darwin GOARCH=amd64 go build -o .out/decrypter-$RELEASE_VERSION-darwin-amd64 cmd/decrypter/main.go
 GOOS=darwin GOARCH=arm64 go build -o .out/decrypter-$RELEASE_VERSION-darwin-arm64 cmd/decrypter/main.go
 GOOS=linux GOARCH=amd64 go build -o .out/decrypter-$RELEASE_VERSION-linux-amd64 cmd/decrypter/main.go
-GOOS=linux GOARCH=arm go build -o .out/decrypter-$RELEASE_VERSION-linux-arm cmd/decrypter/main.go
+GOOS=linux GOARCH=arm64 go build -o .out/decrypter-$RELEASE_VERSION-linux-arm64 cmd/decrypter/main.go
 GOOS=windows GOARCH=amd64 go build -o .out/decrypter-$RELEASE_VERSION-windows-amd64 cmd/decrypter/main.go
